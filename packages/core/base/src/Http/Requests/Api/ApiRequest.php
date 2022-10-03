3589cf135581b7d206f7d8749b582f4527d080c4
ThaiLe <thailh@vietmoney.vn>
2022-10-03 22:46:49 +0700

    support response errorCode when call api

--- packages/core/base/src/Http/Requests/Api/ApiRequest.php
+++ packages/core/base/src/Http/Requests/Api/ApiRequest.php
@@ -1,8 +1,10 @@
 <?php
 namespace Messi\Base\Http\Requests\Api;
 
-
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Arr;
 
 class ApiRequest extends FormRequest
 {
@@ -14,5 +16,37 @@ class ApiRequest extends FormRequest
     public function authorize(): bool
     {
         return true;
+    }
+
+    /**
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $failed = $validator->failed();
+        $field = array_key_first($failed);
+        $errorCode = $this->errorCode();
+        $keyValidate = strtolower(array_key_first(Arr::get($failed, $field)));
+        throw new HttpResponseException(response([
+            'errorCode' => Arr::get($errorCode, $field . '.' . $keyValidate, 9999)
+        ]));
+    }
+
+    /**
+     * @return array
+     */
+    private function errorCode(): array
+    {
+        return [
+            'email' => [
+                'unique' => 1000,
+                'email' => 1001
+            ],
+            'password' => [
+                'required' => 1002,
+                'min' => 1003
+            ],
+        ];
     }
 }
